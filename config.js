f6aee4641c948f2cfe851a73146d75ee9073c1be
natanmorais23 <natanbastosdemorais7@gmail.com>
2024-11-07 12:27:55 -0300

    feat(notifications): add notifications 1/2

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const LOCAL_IP = 'http://192.168.0.102:3000'; // Substitua pelo IP da sua máquina
+const LOCAL_IP = 'http://10.108.134.48:3000'; // Substitua pelo IP da sua máquina
 export default LOCAL_IP;
--- src/app/notifications.tsx
+++ src/app/notifications.tsx
@@ -1,10 +1,19 @@
 import { Text, TouchableOpacity, View } from 'react-native';
 import BackArrow from '../components/backArrow';
+import { Footer } from '../components/footer';
 
 export default function Notifications() {
   return (
-  <View>
-    <BackArrow />
-    <Text>NOTIFICATIONS </Text>
+  <View className='flex-1'>
+    <View className='flex-1'>
+      <BackArrow />
+
+      <View className='bg-white-80 w-[90%] mx-auto rounded-xl p-4 shadow-sm'>
+        <Text className='text-black font-medium text-2xl ml-4'>Campus Eats</Text>
+        <Text className='text-black-gray font-regular text-xl ml-4'>Seu pedido saiu para entrega!</Text>
+      </View>
+
+    </View>
+    <Footer/>
   </View>);
 }
