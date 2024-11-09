f0e8a8814210b6de29f53c261a37102a94f984e5
thiago <thiagoviniciusc33@gmail.com>
2024-11-09 10:44:57 -0300

    style(orderProgress): finalize visual design of order progress screen

--- src/app/orderProgress.tsx
+++ src/app/orderProgress.tsx
@@ -1,9 +1,10 @@
-import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, View, StyleSheet, StatusBar, Image } from 'react-native';
 import { useEffect, useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
-import { useRouter } from 'expo-router'; // Importando a navegação do expo-router
+import { useRouter } from 'expo-router';
 import axios from 'axios';
 import LOCAL_IP from '@/config';
+import { AntDesign, Entypo, Feather, FontAwesome5, FontAwesome6, MaterialCommunityIcons } from '@expo/vector-icons';
 
 interface CartItem {
     id: string;
@@ -42,10 +43,8 @@ export default function OrderProgress() {
     async function handleOrderReceived(orderId: string) {
         try {
             await axios.delete(`${LOCAL_IP}/orders/${orderId}`);
-            // Atualiza o estado para remover o pedido localmente
             setOrders(prevOrders => prevOrders.filter(order => order.id !== orderId));
-            // Navega de volta para a tela inicial
-            router.push('/'); // Substitua '/' pelo caminho correto para a tela inicial, se necessário
+            router.push('/');
         } catch (error) {
             console.error('Erro ao deletar o pedido:', error);
         }
@@ -67,7 +66,11 @@ export default function OrderProgress() {
 
     return (
         <View style={styles.container}>
-            <Text>PROGRESSO DO PEDIDO</Text>
+            <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
+
+            <TouchableOpacity className='absolute z-10 px-5 py-2 ml-2 mt-4 bg-red-main rounded-2xl' onPress={() => handleOrderReceived(orders[0].id)}>
+                    <Text className='text-white'>Voltar</Text>
+            </TouchableOpacity>
             <MapView
                 mapType="standard"
                 showsCompass={false}
@@ -87,14 +90,64 @@ export default function OrderProgress() {
                     />
                 )}
             </MapView>
-            {orders.length > 0 && (
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => handleOrderReceived(orders[0].id)}
+            <View className='w-full h-72 bg-white rounded-t-3xl -mt-9 flex flex-col items-center py-4 '
+                style={{
+                    backgroundColor: 'white',
+                    shadowColor: '#000',
+                    shadowOffset: { width: 0, height: -3 },
+                    shadowOpacity: 0.3,
+                    shadowRadius: 4,
+                    elevation: 10, 
+                }}
+            >
+                <View className="-mt-20 w-28 h-28 rounded-2xl bg-slate-500 overflow-hidden z-10">
+                    <Image
+                        source={{ uri: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png' }}
+                        style={{ width: '100%', height: '100%' }}
+                    />
+                </View>
+                <View className='flex flex-row items-center justify-center gap-2'>
+                    <Text className='font-bold'>Matheus Alves</Text>
+                    <View className='flex-row items-center justify-center'>
+                        <Entypo name="star" size={24} color="#FF9633" />
+                        <Text className='font-medium'>4.8</Text>
+                    </View>
+                </View>
+                <Text className='text-xs text-center font-medium'>HONDA CG 150 | HTX-5738</Text>
+
+                <View className='h-full w-full flex flex-col gap-3 items-center mt-4 p-2'
+                    style={{
+                        backgroundColor: 'white',
+                        shadowColor: '#000',
+                        shadowOffset: { width: 0, height: -3 },
+                        shadowOpacity: 0.3,
+                        shadowRadius: 4, 
+                        elevation: 10, 
+                    }}
                 >
-                    <Text style={styles.buttonText}>Pedido Recebido</Text>
-                </TouchableOpacity>
-            )}
+                    <View className='flex-row gap-1'>
+                        <Feather name="check-circle" size={24} color="#EF2A39" />
+                        <Text className='font-bold text-red-main'>.....</Text>
+                        <MaterialCommunityIcons name="chef-hat" size={24} color="#EF2A39" />
+                        <Text className='font-bold text-red-main'>.....</Text>
+                        <FontAwesome6 name="box" size={24} color="#EF2A39" />
+                        <Text className='font-bold text-red-main'>.....</Text>
+                        <FontAwesome6 name="motorcycle" size={24} color="#EF2A39" />
+                        <Text className='font-bold text-red-main'>.....</Text>
+                        <FontAwesome5 name="hands-helping" size={24} color="#EF2A39" style={{ opacity: 0.3 }}/>
+
+                    </View>
+                    <View className='flex-col items-center'>
+                        <Text className='font-medium'>O entregador está a caminho...</Text>
+                        <Text className='font-medium'>Tempo estimado: 2min</Text>
+                    </View>
+                    
+                    <TouchableOpacity className='px-5 py-2 ml-2 mt-4 bg-red-main rounded-xl' onPress={() => handleOrderReceived(orders[0].id)}>
+                        <MaterialCommunityIcons name="chat-plus" size={24} color="white" />
+                    </TouchableOpacity>
+                </View>
+            </View>
+            
         </View>
     );
 }
@@ -106,16 +159,6 @@ const styles = StyleSheet.create({
     },
     map: {
         flex: 1,
-    },
-    button: {
-        position: 'absolute',
-        bottom: 20,
-        left: '10%',
-        right: '10%',
-        backgroundColor: '#007AFF',
-        padding: 15,
-        borderRadius: 10,
-        alignItems: 'center',
     },
     buttonText: {
         color: 'white',
