a87c815df3310176fbc8398bc443141ec925b4b7
Thiago Lopes <thiagoviniciusc33@gmail.com>
2024-11-13 10:15:08 -0300

    feat(orderHistoryScreen)

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const LOCAL_IP = 'http://10.104.133.246:3000'; // Substitua pelo IP da sua máquina
+const LOCAL_IP = 'http:/10.106.214.2:3000'; // Substitua pelo IP da sua máquina
 export default LOCAL_IP;
