e40c48801d64deea9cfddb526d8ed32948bb9e1b
Thiago Lopes <thiagoviniciusc33@gmail.com>
2024-11-26 18:51:19 -0300

    feature(orders) map orders from db

--- src/app/orders.tsx
+++ src/app/orders.tsx
@@ -38,11 +38,23 @@ interface Restaurant {
     logo: string;
 }
 
+interface FoodItem {
+    id: string;
+    name: string;
+    restaurantId: string;
+    rating: string;
+    isFavorite: boolean;
+    category: string;
+    url: string;
+    price: number;
+}
+
 export default function Orders() {
   const [pendingOrders, setPendingOrders] = useState<Order[]>([]);
   const [deliveredOrders, setDeliveredOrders] = useState<Order[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [restaurants, setRestaurants] = useState<Map<string, Restaurant>>(new Map());
+  const [foodItems, setFoodItems] = useState<Map<string, FoodItem>>(new Map());
 
   useEffect(() => {
     async function fetchOrders() {
@@ -53,7 +65,6 @@ export default function Orders() {
         setPendingOrders(allOrders.filter((order) => order.status === 'Pendente'));
         setDeliveredOrders(allOrders.filter((order) => order.status === 'Entregue'));
 
-        // Buscar restaurantes
         const restaurantIds = new Set(allOrders.flatMap((order) => order.items.map((item) => item.restaurantId)));
         const restaurantRequests = Array.from(restaurantIds).map((restaurantId) =>
           axios.get<Restaurant>(`${LOCAL_IP}/restaurants/${restaurantId}`)
@@ -65,8 +76,20 @@ export default function Orders() {
           const restaurant = response.data;
           restaurantMap.set(restaurant.id, restaurant);
         });
-
         setRestaurants(restaurantMap);
+
+		const foodIds = new Set(allOrders.flatMap((order) => order.items.map((item) => item.foodId)));
+        const foodRequests = Array.from(foodIds).map((foodId) =>
+          axios.get<FoodItem>(`${LOCAL_IP}/products/${foodId}`)
+        );
+
+        const foodResponses = await Promise.all(foodRequests);
+        const foodMap = new Map<string, FoodItem>();
+        foodResponses.forEach((response) => {
+          const foodItem = response.data;
+          foodMap.set(foodItem.id, foodItem);
+        });
+        setFoodItems(foodMap);
       } catch (error) {
         console.error('Erro ao buscar pedidos:', error);
       } finally {
@@ -88,88 +111,108 @@ export default function Orders() {
 
   return (
     <View style={styles.container}>
-      <BackArrow color="black" title="Meus pedidos" route="/" />
-      <View style={styles.content}>
-        {pendingOrders.length > 0 && (
-          <View>
-			<View className="-mb-8 w-36 h-36 rounded-full bg-slate-500 overflow-hidden z-10 mx-auto">
-				<Image
-					source={{ uri: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png' }}
-					style={{ width: '100%', height: '100%' }}
-				/>
-			</View>
-			<View className='pt-10' style={styles.card}>
-				<View className='flex-row items-center justify-center'>
-					<Entypo name="dot-single" size={24} color="red" />
-					<Text className='font-semibold text-lg'>Pedido em preparação</Text>
-				</View>
-				<FlatList
-				data={pendingOrders}
-				keyExtractor={(item) => item.id}
-				renderItem={({ item }) => (
-					<View >
-					<Text>Pedido ID: {item.id}</Text>
+      	<BackArrow color="black" title="Meus pedidos" route="/" />
+      	<View style={styles.content}>
+	    <FlatList
+			data={pendingOrders}
+			keyExtractor={(item) => item.id.toString()}
+			renderItem={({ item }) => (
+				<View className='bg-slate-300'>
+					<View className="-mb-8 w-36 h-36 rounded-full bg-slate-500 overflow-hidden z-10 mx-auto">
+						<Image
+							source={{ uri: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png' }}
+							style={{ width: '100%', height: '100%' }}
+						/>
+					</View>
+					<Text className='pt-8'>Pedido ID: {item.id}</Text>
 					<FlatList
 						data={item.items}
-						keyExtractor={(cartItem) => cartItem.id}
-						renderItem={({ item: cartItem }) => (
-						<Text>
-							{cartItem.quantity}x Produto: {cartItem.foodId}
-						</Text>
-						)}
+						keyExtractor={(orderItem) => orderItem.id.toString()}
+						renderItem={({ item: orderItem }) => {
+							// Obtenha o restaurante usando o restaurantId
+							const restaurant = restaurants.get(orderItem.restaurantId);
+							return (
+								<Text>
+								{orderItem.quantity}x Produto: {orderItem.foodId}
+								{restaurant && (
+									<Text> - Restaurante: {restaurant.name}</Text> // Exibe o nome do restaurante
+								)}
+								</Text>
+							);
+						}}
 					/>
-					</View>
-				)}
-				ListEmptyComponent={
-					<Text>Nenhum pedido pendente encontrado.</Text>
-				}
-				/>
-			</View>
-          </View>
-        )}
-
+				</View>
+			)}
+			ListEmptyComponent={
+				<Text>Nenhum pedido pendente encontrado.</Text>
+			}
+	 	/>
         <Text style={{ marginTop: 20 }}>Histórico de Pedidos</Text>
         <FlatList
-          data={deliveredOrders}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <View className="bg-white px-2 py-6" style={styles.card}>
-              {item.items.map((cartItem) => {
-                const restaurant = restaurants.get(cartItem.restaurantId);
-                return (
-                  <View key={cartItem.id} className='flex-row items-center justify-between'>
-                    <View className='flex-row items-center gap-1'>
-                      {restaurant && (
-                        <Image
-                          source={{ uri: restaurant.logo }}
-                          style={{ width: 50, height: 50, borderRadius: 25, marginRight: 10 }}
-                        />
-                      )}
-                      <Text className='font-semibold'>{restaurant?.name} • Nº {item.id}</Text>
-                    </View>
-                    <Entypo name="chevron-right" size={24} color="black" className='mr-4'/>
-                  </View>
-                );
-              })}
-              <View className="w-full h-[1px] mt-4 mb-6 bg-gray-line" />
-              <View className='flex-row px-6 justify-between'>
-                <View className='flex-col'>
-                  <View className='flex-row gap-2'>
-                    <Text className='bg-slate-100 px-1 rounded-sm'>1</Text>
-                    <Text>Hambúrguer</Text>
-                  </View>
-                  <Text>+1 item</Text>
-                </View>
-                <View className='flex items-center justify-center bg-red-main rounded-xl px-4'>
-                  <Text className='text-white'>Pedir novamente</Text>
-                </View>
-              </View>
-            </View>
-          )}
-          ListEmptyComponent={
-            <Text>Nenhum pedido entregue encontrado.</Text>
-          }
-        />
+			data={deliveredOrders}
+			keyExtractor={(item) => item.id.toString()}
+			renderItem={({ item }) => {
+
+				const total = item.items.reduce((sum, orderItem) => {
+				const foodItem = foodItems.get(orderItem.foodId);
+				if (foodItem) {
+					return sum + (foodItem.price * orderItem.quantity);
+				}
+				return sum;
+				}, 0);
+
+				return (
+					<View className="bg-white px-4 py-6 mb-4 rounded-lg shadow-md">
+						{item.items.length > 0 && (
+							<View className="flex-row items-center justify-between mb-4">
+								<View className="flex-row items-center gap-2">
+								{restaurants.get(item.items[0].restaurantId) && (
+									<Image
+									source={{
+										uri: restaurants.get(item.items[0].restaurantId)?.logo,
+									}}
+									style={{ width: 50, height: 50, borderRadius: 25 }}
+									/>
+								)}
+								<Text className="font-semibold text-lg">
+									{restaurants.get(item.items[0].restaurantId)?.name} • Pedido Nº {item.id}
+								</Text>
+								</View>
+								<Entypo name="chevron-right" size={24} color="black" />
+							</View>
+						)}
+
+						<View className="w-full h-[1px] bg-gray-line mb-4" />
+
+						{item.items.map((orderItem) => {
+							const foodItem = foodItems.get(orderItem.foodId);
+							return (
+								<View key={orderItem.id} className="flex-row justify-between items-center mb-2">
+								<View className="flex-row gap-2">
+									<Text className="bg-gray-200 px-2 py-1 rounded text-sm">{orderItem.quantity}x</Text>
+									{foodItem && <Text className="text-gray-700">{foodItem.name}</Text>}
+								</View>
+								<Text className="text-gray-500">
+									R$ {(orderItem.quantity * (foodItem?.price || 0)).toFixed(2)}
+								</Text>
+								</View>
+							);
+						})}
+
+						<View className="flex-row justify-between items-center mt-6">
+						<Text className="font-semibold text-lg">
+							Total: R$ {total.toFixed(2)}
+						</Text>
+						<View className="bg-red-main px-4 py-2 rounded-xl">
+							<Text className="text-white">Pedir novamente</Text>
+						</View>
+						</View>
+					</View>
+				);
+			}}
+			ListEmptyComponent={<Text>Nenhum pedido entregue encontrado.</Text>}
+		/>
+
       </View>
       <Footer />
     </View>
