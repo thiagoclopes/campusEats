c168521b85a8c519d9eecd782c6e222f0a042224
Thiago Lopes <thiagoviniciusc33@gmail.com>
2024-11-30 14:59:02 -0300

    feat(mapscreen) add restaurand and sector pins

--- src/app/mapscreen.tsx
+++ src/app/mapscreen.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { StatusBar, StyleSheet, TouchableOpacity, View, Image, Text } from 'react-native';
 import MapView, { Callout, Marker } from 'react-native-maps';
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign, Entypo, FontAwesome5 } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { router } from 'expo-router';
 import * as Location from 'expo-location';
+import { Asset } from 'expo-asset';
 
 interface LocationCoords {
     latitude: number;
@@ -12,6 +13,13 @@ interface LocationCoords {
 }
 
 interface Restaurant {
+    id: number;
+    name: string;
+    description: string;
+    coords: LocationCoords;
+}
+
+interface Sector {
     id: number;
     name: string;
     description: string;
@@ -32,6 +40,46 @@ const restaurants: Restaurant[] = [
         coords: { latitude: -5.839732, longitude: -35.19570 },
     },
     
+];
+
+
+const Sectors: Sector[] = [
+    {
+        id: 1,
+        name: 'Setor I', 
+        description: 'Próximo à entrada principal e de fácil acesso.',
+        coords: { latitude: -5.8374872488393645, longitude: -35.199658102831926 },
+    },
+    {
+        id: 2,
+        name: 'Setor II',
+        description: 'Área de convivência com mesas e bancos para os estudantes.',
+        coords: { latitude: -5.840452832201686, longitude: -35.19724725069943 },
+    },
+    {
+        id: 3,
+        name: 'Setor III',
+        description: 'Ponto central próximo à administração da UFRN.',
+        coords: { latitude: -5.840709167986833, longitude: -35.20058612478776 },
+    },
+    {
+        id: 4,
+        name: 'Setor IV', 
+        description: 'Perto do Instituto de Física e Departamento de Matemática.',
+        coords: { latitude: -5.842412943228999, longitude: -35.19972013385473 },
+    },
+    {
+        id: 5,
+        name: 'Escola de Ciência e Tecnologia (ECT)',
+        description: 'Área com movimentação frequente de estudantes.',
+        coords: { latitude: -5.843395493045956, longitude: -35.199395317946305 },
+    },
+    {
+        id: 6,
+        name: 'Setor V', 
+        description: '.',
+        coords: { latitude: -5.838337951119542, longitude: -35.19796197544989 },
+    },
 ];
 
 export default function MapScreen() {
@@ -69,6 +117,17 @@ export default function MapScreen() {
         longitudeDelta: 0.01,
     };
 
+    const customMapStyle = [
+        { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#e0e0e0" }] },
+        { "featureType": "road", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }] },
+        { "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#b0d0d0" }] },
+        { "featureType": "poi", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }
+    ];
+
+    const customRestaurantPin = Asset.fromModule(require('../assets/custom_pin.png')).uri;
+    const customSectorPin = Asset.fromModule(require('../assets/custom_sector_pin.png')).uri;
+    const [showView, setShowView] = useState(false);
+
     return (
         <View style={styles.container}>
             <StatusBar backgroundColor="white" barStyle="dark-content" />
@@ -77,32 +136,55 @@ export default function MapScreen() {
             </TouchableOpacity>
 
             <MapView
-                mapType="hybrid"
+                mapType="standard"
                 showsCompass={false}
                 style={styles.map}
                 initialRegion={initialRegion}
                 showsUserLocation={true}
+                customMapStyle = {customMapStyle}
             >
                 {restaurants.map((restaurant) => (
                     <Marker
-                    key={restaurant.id}
-                    coordinate={restaurant.coords}
-                    title={restaurant.name}
-                    description={restaurant.description}
-                >
-                    <View className="items-center">
-                        <Text className="font-bold text-lg mb-1 text-red-main bg-white/50">
-                            {restaurant.name}
-                        </Text>
-                        <FontAwesome name="map-pin" size={28} color="red" />
-                        {/*<Image
-                            source={{ uri: 'https://pbs.twimg.com/media/FTEEVe8XoAAiyb5.jpg:large' }}
-                            className="w-16 h-16 object-contain"
-                        />*/}
-                    </View>
-                </Marker>
+                        key={restaurant.id}
+                        coordinate={restaurant.coords}
+                        title={restaurant.name}
+                        description={restaurant.description}
+                        onPress={() => {
+                            setShowView(true);
+                        }}
+                        onDeselect={() => {
+                        // Esconde a view quando o marcador é desmarcado
+                        setShowView(false);
+                        }}
+                    >
+                        <Image 
+                            source={{ uri: customRestaurantPin }}
+                            style={{ width: 35, height: 35 }}
+                            resizeMode="contain"
+                        />
+                    </Marker>
+                ))}
+                {Sectors.map((sector) => (
+                    <Marker
+                        key={sector.id}
+                        coordinate={sector.coords}
+                        title={sector.name}
+                    >
+                        <View>
+                            <Image 
+                                source={{ uri: customSectorPin }}
+                                style={{ width: 35, height: 35 }}
+                                resizeMode="contain"
+                            />
+                        </View>
+                    </Marker>
                 ))}
             </MapView>
+            {showView && (
+                <View style={styles.bottomView}>
+                <Text>Informações do marcador</Text>
+                </View>
+            )}
         </View>
     );
 }
@@ -114,4 +196,13 @@ const styles = StyleSheet.create({
     map: {
         flex: 1,
     },
+    bottomView: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'white',
+        padding: 20,
+        elevation: 5,
+      },
 });
