b0dac818ef810eba9f7ed62d9c6c71d19680223d
thiago <thiagoviniciusc33@gmail.com>
2024-12-28 10:51:40 -0300

    fix(index.tsx): change fetchPendingOrder param

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -78,7 +78,7 @@ const fetchFoodItem = async (foodId: string) => {
 const fetchPendingOrder = async () => {
 	try {
 		const response = await axios.get<Order[]>(`${LOCAL_IP}/orders`, {
-			params: { status: "Pendente" },
+			params: { status: "Preparing" },
 		});
 		return response.data;
 
