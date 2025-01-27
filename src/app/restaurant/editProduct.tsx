59c176d72ab0d2bff127ccb0b5d6b41aa8117ba3
thiago <thiagoviniciusc33@gmail.com>
2025-01-27 10:25:25 -0300

    feat(restaurant): create initial structure for restaurant order management page

---
+++ src/app/restaurant/editProduct.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+export default function editProduct() {
+    return (
+        <View></View>
+    )
+}
--- src/app/restaurant/orders.tsx
+++ src/app/restaurant/orders.tsx
@@ -1,15 +1,61 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { Footer } from "@/src/components/restaurant/footer";
+import BackArrow from "@/src/components/shared/backArrow";
+import { useState } from "react";
 
 export default function Orders() {
-  return (
-    <View className="flex-1">
-      <View className="flex-1 justify-center">
-        <Text style={{ fontSize: 24, fontWeight: "bold" }}>
-          Meus pedidos
-        </Text>
-      </View>
-      <Footer/>
-    </View>
-  );
+	type TabOptions = 'novos' | 'em-preparo' | 'prontos';
+	const [selectedTab, setSelectedTab] = useState<TabOptions>('novos');
+
+	const renderContent = () => {
+		switch (selectedTab) {
+			case 'novos':
+				return <View><Text>Conteúdo para Novos</Text></View>;
+			case 'em-preparo':
+				return <View><Text>Conteúdo para Em Preparo</Text></View>;
+			case 'prontos':
+				return <View><Text>Conteúdo para Prontos</Text></View>;
+			default:
+				return null;
+		}
+	};
+
+	return (
+	<View className="flex-1">
+		<View className="flex-1">
+			<BackArrow color="black" title="Pedidos" route="/restaurant" />
+			<View className="w-full flex flex-row">
+				<TouchableOpacity
+					className={`flex-1 items-center justify-center p-4 border-b-2 ${
+						selectedTab === 'novos' ? 'border-black' : 'border-gray-line'
+					}`}
+					onPress={() => setSelectedTab('novos')}
+				>
+					<Text>Novos</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+					className={`flex-1 items-center justify-center p-4 border-b-2 ${
+						selectedTab === 'em-preparo' ? 'border-black' : 'border-gray-line'
+					}`}
+					onPress={() => setSelectedTab('em-preparo')}
+				>
+					<Text>Em preparo</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+					className={`flex-1 items-center justify-center p-4 border-b-2 ${
+						selectedTab === 'prontos' ? 'border-black' : 'border-gray-line'
+					}`}
+					onPress={() => setSelectedTab('prontos')}
+				>
+					<Text>Prontos</Text>
+				</TouchableOpacity>
+			</View>
+
+			<View className="mt-4">
+				{renderContent()}
+			</View>
+		</View>
+		<Footer/>
+	</View>
+	);
 }
