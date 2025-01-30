af93b81fa5bf5454539ac5740c0e3f290a4937d0
Thiago Lopes <thiagoviniciusc33@gmail.com>
2025-01-29 22:03:28 -0300

    chore(json-server): update to 0.17.4

---
+++ server.js
@@ -0,0 +1,28 @@
+const jsonServer = require("json-server");
+const path = require("path");
+const server = jsonServer.create();
+const dbPath = path.join(__dirname, "db.json");
+const router = jsonServer.router(dbPath);
+const middlewares = jsonServer.defaults();
+
+server.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	next();
+  });
+
+server.use((req, res, next) => {
+console.log(`Recebida requisição: ${req.method} ${req.url}`);
+next();
+});
+
+server.use(middlewares);
+
+server.use(router);
+
+server.listen(3000, () => {
+	console.log(`JSON Server is running`);
+});
+
+module.exports = server;
